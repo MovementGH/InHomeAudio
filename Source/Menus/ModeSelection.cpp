e9e2fa9a40f0636c9b22b8abb5b28b944ecfab4f
MovementGH <48330137+MovementGH@users.noreply.github.com>
2019-08-08 14:35:58 -0400

    Change Voice Call Font Improve Fade In

--- Source/Menus/ModeSelection.cpp
+++ Source/Menus/ModeSelection.cpp
@@ -11,17 +11,20 @@ namespace Menus {
         m_Title.setString("In Home Audio");
         m_Title.setOrigin(m_Title.getLocalBounds().width/2,0);
         m_Line.setPosition(0,117);
-        if(isMobile()) {
-            m_Background.setColor(sf::Color(128,128,128,16));
-            m_Title.setFillColor(sf::Color(255,255,255,4));
-        }
-        else
-            m_Background.setColor(sf::Color(128,128,128,255));
         m_ModeSprites.resize(10);
         for(int i=0;i<m_ModeSprites.size();i++) {
             m_ModeSprites[i].setTexture(m_VoiceTexture);
             m_ModeSprites[i].setOrigin(m_VoiceTexture.getSize().x/2,m_VoiceTexture.getSize().y/2);
         }
+        if(isMobile()) {
+            m_Background.setColor(sf::Color(128,128,128,16));
+            m_Title.setFillColor(sf::Color(255,255,255,4));
+            m_Line.setFillColor(sf::Color(255,255,255,4));
+            for(int i=0;i<m_ModeSprites.size();i++)
+                m_ModeSprites[i].setColor(sf::Color(255,255,255,4));
+        }
+        else
+            m_Background.setColor(sf::Color(128,128,128,255));
     }
     void ModeSelection::createMenu(sf::Vector2u WindowSize) {
         if(((float)WindowSize.x/(float)WindowSize.y)<1)
@@ -48,7 +51,12 @@ namespace Menus {
     }
     void ModeSelection::onForeground(sf::Time Delta) {
         if(m_Background.getColor().a<255&&Delta.asSeconds()<.1) m_Background.setColor(sf::Color(128,128,128,std::min(m_Background.getColor().a*(1+10*Delta.asSeconds()),255.f)));
-        else if(m_Title.getFillColor().a<255&&Delta.asSeconds()<.1) m_Title.setFillColor(sf::Color(255,255,255,std::min(m_Title.getFillColor().a*(1+20*Delta.asSeconds()),255.f)));
+        else if(m_Title.getFillColor().a<255&&Delta.asSeconds()<.1) {
+            m_Title.setFillColor(sf::Color(255,255,255,std::min(m_Title.getFillColor().a*(1+20*Delta.asSeconds()),255.f)));
+            m_Line.setFillColor(sf::Color(255,255,255,std::min(m_Line.getFillColor().a*(1+20*Delta.asSeconds()),255.f)));
+            for(int i=0;i<m_ModeSprites.size();i++)
+                m_ModeSprites[i].setColor(sf::Color(255,255,255,std::min(m_ModeSprites[i].getColor().a*(1+20*Delta.asSeconds()),255.f)));
+        }
         m_Manager->getWindow().draw(m_Background);
         m_Manager->getWindow().draw(m_Title);
         m_Manager->getWindow().draw(m_Line);
