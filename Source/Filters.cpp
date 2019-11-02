55afb88a49e8037d13f114234de82d15f3a4c87c
MovementGH <48330137+MovementGH@users.noreply.github.com>
2019-11-02 15:45:50 -0400

    Add Noise Filter

--- Source/Filters.cpp
+++ Source/Filters.cpp
@@ -3,3 +3,10 @@
 bool AudioFilter::Filter(std::vector<sf::Int16> &Samples) {
     return true;
 }
+
+NoiseFilter::NoiseFilter(int QuietLevel):m_Quiet(QuietLevel){}
+bool NoiseFilter::Filter(std::vector<sf::Int16> &Samples) {
+    bool Noise=false;
+    for(int i=0;i<Samples.size();i++) if(Samples[i]>m_Quiet||Samples[i]<-m_Quiet) Noise=true;
+    return Noise;
+}
--- Source/Filters.hpp
+++ Source/Filters.hpp
@@ -8,4 +8,12 @@ public:
     bool Filter(std::vector<sf::Int16>& Samples);
 };
 
+class NoiseFilter {
+public:
+    NoiseFilter(int QuietLevel=0);
+    bool Filter(std::vector<sf::Int16>& Samples);
+protected:
+    int m_QuietLevel;
+};
+
 #endif /* Filters_hpp */
